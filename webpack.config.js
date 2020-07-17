f79c17a781bc89d130db75b7fa8348265b615e68
Juan Goez <juan.goez@upb.edu.co>
2020-07-17 10:43:17 -0500

    webpack configuration implemented

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: __dirname + "/build",
+    filename: "bundle.js",
+  },
+  devServer: {
+    port: 3000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "sass-loader" },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "bundle.css",
+    }),
+  ],
+};
